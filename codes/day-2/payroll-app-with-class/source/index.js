13eb9e83f3581c5159ac9ea88c23b1f56e713842
Joydip <joydip.mondal@gmail.com>
2022-11-16 09:15:18 +0530

    day-2 upload final

--- codes/day-2/payroll-app-with-class/source/index.js
+++ codes/day-2/payroll-app-with-class/source/index.js
@@ -1,14 +1,30 @@
+const successFn = function (status) {
+    console.log(status ? 'added' : 'not')
+    console.log(employees)
+}
+const failureFn = function (e) {
+    console.log(e.message)
+}
+
 const anilEmployee = new Developer('anil', 1, 1000, 2000, 3000, 4000)
 anilEmployee.calculateSalary()
-console.log(addEmployee(anilEmployee) === true ? 'added' : 'not')
+addEmployee(anilEmployee).then(successFn, failureFn)
+//console.log(addEmployee(anilEmployee) === true ? 'added' : 'not')
 
 const joyEmployee = new Hr('joydip', 2, 2000, 3000, 4000, 5000)
 joyEmployee.calculateSalary()
-console.log(addEmployee(joyEmployee) === true ? 'added' : 'not')
+addEmployee(joyEmployee).then(successFn, failureFn)
+//console.log(addEmployee(joyEmployee) === true ? 'added' : 'not')
 
-const filteredArray = filterEmployees(12000)
-if (filteredArray !== undefined && filteredArray.length > 0) {
-    console.log(filteredArray)
-}
+//const filteredArray = filterEmployees(12000)
+filterEmployees(12000)
+    .then(
+        function (filteredArray) {
+            if (filteredArray !== undefined && filteredArray.length > 0) {
+                console.log(filteredArray)
+            }
+        },
+        failureFn
+    )
 
-console.log(employees)
+
--- codes/day-2/payroll-app-with-class/source/manager/manager.js
+++ codes/day-2/payroll-app-with-class/source/manager/manager.js
@@ -1,29 +1,48 @@
 function addEmployee(empObj) {
-    if (employees) {
-        let index = -1
-        if (employees.length > 0) {
-            index = employees.findIndex(
-                function (emp) {
-                    return emp.id === empObj.id
-                }
-            )
-        }
+    return new Promise(
+        function (resolveFn, rejectFn) {
+            if (employees) {
+                try {
+                    let index = -1
+                    if (employees.length > 0) {
+                        index = employees.findIndex(
+                            function (emp) {
+                                return emp.id === empObj.id
+                            }
+                        )
+                    }
 
-        if (index === -1 || employees.length === 0) {
-            employees.push(empObj)
-            return true
-        } else
-            return false
-    } else
-        throw new Error('repository not defined')
+                    if (index === -1 || employees.length === 0) {
+                        employees.push(empObj)
+                        resolveFn(true)
+                    } else
+                        resolveFn(false)
+                } catch (error) {
+                    rejectFn(error)
+                }
+            } else
+                rejectFn(new Error('repository not defined'))
+        }
+    )
 }
 
 function filterEmployees(salary) {
-    let result = []
-    if (employees.length > 0) {
-        result = employees.filter(function (emp) {
-            return emp.totalSalary > salary
-        })
-    }
-    return result
+    return new Promise(
+        function (resolveFn, rejectFn) {
+            if (employees) {
+                try {
+                    let result = []
+                    if (employees.length > 0) {
+                        result = employees.filter(function (emp) {
+                            return emp.totalSalary > salary
+                        })
+                    }
+                    resolveFn(result)
+                } catch (error) {
+                    rejectFn(error)
+                }
+            } else
+                rejectFn(new Error('repository not defined'))
+        }
+    )
 }
